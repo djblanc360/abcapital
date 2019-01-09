751659e9d831cc3f82e5546266906a6a4008cd38
djblanc360 <djblanc360@gmail.com>
2019-01-08 19:28:44 -0800

    post format in news

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ get_header();
 
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 				<div class="row">
-			    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+			    <div class="col-xs-2 col-sm-2">
 
 						<?php echo the_post_thumbnail(); ?>
 				  </div>
