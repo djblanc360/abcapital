e05d3fc57abd5f67faaa1dc57f9ec614c68f1490
djblanc360 <djblanc360@gmail.com>
2019-01-08 19:04:40 -0800

    post format in news

--- index.php
+++ index.php
@@ -22,8 +22,23 @@ get_header();
 
 			    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
 
+						<?php echo the_post_thumbnail(); ?>
+						<h6 class="post-title"><a href="<?php the_permalink(); ?>" class="post-title-link"><?php the_title(); ?></a></h6>
+                <p>
+                    <?php $postLikes = wp_ulike_get_post_likes(get_the_ID());?>
+                     <i class="far fa-calendar-alt"></i> <?php echo get_the_date('M d, Y'); ?> | <i class="far fa-comments"></i> <?php echo get_comments_number(); ?> | <i class="far fa-heart"></i> <?php if($postLikes) { echo $postLikes ; } else { echo '<span>0</span>'; } ?>
+                </p>
 
-					</div> 
+        <div class="post-text"> <?php the_excerpt(); ?> </div>
+    		<a href="<?php the_permalink(); ?>" class="button read-more-button">Read More</a>
+		    <div class="clearfix"></div>
+            <p class="blogPostMeta">
+                <?php the_author(); ?>  | <?php foreach((get_the_category()) as $category) { echo $category->cat_name . ' '; } ?>
+            </p>
+        <div class="clearfix"></div>
+										
+
+					</div>
 
 			    <?php endwhile; ?>
 			  <?php  else : ?>
