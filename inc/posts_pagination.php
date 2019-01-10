f2c8d9109270b63f27ce716651e833a3342f90a5
djblanc360 <djblanc360@gmail.com>
2019-01-09 18:42:00 -0800

    added numbered pagination

--- inc/posts_pagination.php
+++ inc/posts_pagination.php
@@ -1,16 +1,12 @@
 <?php
-if ( ! function_exists( 'numbered_pagination' ) ) :
-    function numbered_pagination() {
-        global $wp_query;
+global $wp_query;
 
-        $big = 999999999; // need an unlikely integer
+$big = 999999999; // need an unlikely integer
 
-        echo paginate_links( array(
-            'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-            'format' => '?paged=%#%',
-            'current' => max( 1, get_query_var('paged') ),
-            'total' => $wp_query->max_num_pages
-        ) );
-    }
-endif;
+echo paginate_links( array(
+	'base' => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+	'format' => '?paged=%#%',
+	'current' => max( 1, get_query_var('paged') ),
+	'total' => $wp_query->max_num_pages
+) );
 ?>
