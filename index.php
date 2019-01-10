ba60cc25b67cbfc03d943a2e94ea9d614431528f
djblanc360 <djblanc360@gmail.com>
2019-01-09 17:37:09 -0800

    changed layout for category accordion

--- index.php
+++ index.php
@@ -20,17 +20,17 @@ get_header();
 
 <div class="row">
 	<div class="col-sm-3">
-		
+
 	</div><!-- END CATEGORY -->
 	<div class="col-sm-7">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			<div class="row">
-				<div class="col-xs-6 col-sm-2">
+				<div class="col-xs-6 ">
 
 					<?php echo the_post_thumbnail(); ?>
 				</div>
 
-				<div class="col-xs-12 col-sm-6">
+				<div class="col-xs-12 ">
 
 
 					<h4 class="post-title"><a href="<?php the_permalink(); ?>" class="post-title-link"><?php the_title(); ?></a></h4>
