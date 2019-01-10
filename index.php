1f4042d913a102fc40060acb9e7da05c2954d421
djblanc360 <djblanc360@gmail.com>
2019-01-10 05:32:46 -0800

    posts categories

--- index.php
+++ index.php
@@ -20,7 +20,31 @@ get_header();
 
 <div class="row news-category-column">
 	<div class="col-sm-2">
-<?php wp_dropdown_categories( $args ); ?> 		
+		<?php
+		$custom_terms = get_terms('custom_taxonomy');
+
+		foreach($custom_terms as $custom_term) {
+		    wp_reset_query();
+		    $args = array('post_type' => 'custom_post_type',
+		        'tax_query' => array(
+		            array(
+		                'taxonomy' => 'custom_taxonomy',
+		                'field' => 'slug',
+		                'terms' => $custom_term->slug,
+		            ),
+		        ),
+		     );
+
+		     $loop = new WP_Query($args);
+		     if($loop->have_posts()) {
+		        echo '<h2>'.$custom_term->name.'</h2>';
+
+		        while($loop->have_posts()) : $loop->the_post();
+		            echo '<a href="'.get_permalink().'">'.get_the_title().'</a><br>';
+		        endwhile;
+		     }
+		}
+		?>
 		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
 		    <div class="panel panel-default">
 		        <div class="panel-heading" role="tab" id="headingOne">
