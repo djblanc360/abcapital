28d91a6e135a24a612b46c665ca51154195663d4
djblanc360 <djblanc360@gmail.com>
2019-01-09 17:47:08 -0800

    changed layout for category accordion

--- index.php
+++ index.php
@@ -20,7 +20,53 @@ get_header();
 
 <div class="row">
 	<div class="col-sm-3">
+		<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+		    <div class="panel panel-default">
+		        <div class="panel-heading" role="tab" id="headingOne">
+		             <h4 class="panel-title">
+		        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+		          Category Group 1
+		        </a>
+		      </h4>
 
+		        </div>
+		        <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+		            <div class="panel-body">
+
+								</div>
+		        </div>
+		    </div>
+		    <div class="panel panel-default">
+		        <div class="panel-heading" role="tab" id="headingTwo">
+		             <h4 class="panel-title">
+		        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+		          Category Group 2
+		        </a>
+		      </h4>
+
+		        </div>
+		        <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+		            <div class="panel-body">
+
+								</div>
+		        </div>
+		    </div>
+		    <div class="panel panel-default">
+		        <div class="panel-heading" role="tab" id="headingThree">
+		             <h4 class="panel-title">
+		        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+		          Category Group 3
+		        </a>
+		      </h4>
+
+		        </div>
+		        <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+		            <div class="panel-body">
+		            	
+		            </div>
+		        </div>
+		    </div>
+		</div>
 	</div><!-- END CATEGORY -->
 	<div class="col-sm-8">
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
