5eecd61f4cfd55a9cf7b4ffc0b64b3edc9d84c23
djblanc360 <djblanc360@gmail.com>
2019-01-18 22:20:01 -0800

    testing

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
 
 get_header();
 ?>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
